d9c9b13e51098a85b0d26e8f598b42e8e1051689
stephenstuart17 <stephen.stuart@oracle.com>
2023-04-26 11:20:11 -0500

    PythonDemo

--- python/pythonoracledbvsords/walletcredentials.py
+++ python/pythonoracledbvsords/walletcredentials.py
@@ -2,10 +2,10 @@
 # The username for the user accessing the database. In this case, I created a separate "devuser". Just make sure, whatever it is, you use it for all three
 # tests (otherwise it could become an issue when you get to the OAuth2.0 client steps in "test3.py" 
 # uname = "[user name]"
-uname = ""
+uname = "ADMIN"
 
 # pwd = "[the password for the above user]"
-pwd = ""
+pwd = "Jimmy316109!"
 
 # Review my "test1.py" file to see how I have this set-up. I have installed the "Path Intellisense" VS Code extension for simplifying entering path and file
 # names. So if it looks weird, that is why. 
@@ -23,11 +23,11 @@ wltloc = "./config_dir/"
 # "password1234" you knucklehead. 
 # wpwd = "[Your wallet password]"
 
-wltpwd = ""
+wltpwd = "password1234"
 
 # DSN stands for dataservice name. These can all be found in your tnsnames.ora file. You'll see three options: "[Your DB name]_high", "[Your DB name]_low",
 # '[Your DB name]_medium"
 
 # dsn = "[Service name]"
 
-dsn = ""
+dsn = "pythondemo_medium"
